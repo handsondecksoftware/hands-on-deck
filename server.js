0ae89ce7c0c3a7a395e6989d53ca3a0c36ca077d
themennice <dziubiiden@gmail.com>
2020-06-05 03:11:57 -0700

    added heroku db

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 const express = require('express')
 const app = express()
+const { Client } = require('pg');
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+// Heroku dynamically assigns a port to the app
+// so we cannot set the port to a fixed number, use an OR statement instead
+// since Heroku ads PORT to env
 app.listen(process.env.PORT || 5000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 5000!')
 })
+
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
