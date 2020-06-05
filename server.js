624baadf58283eeaa5c9bca4d534bbb955a96cd2
themennice <dziubiiden@gmail.com>
2020-06-05 02:53:51 -0700

    creating a heroku server

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var http = require('http');
+const express = require('express')
+const app = express()
 
 //create a server object:
 http.createServer(function (req, res) {
@@ -6,3 +8,13 @@ http.createServer(function (req, res) {
   res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
 }).listen(8080); //the server object listens on port 8080
+
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
