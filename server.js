1fd684c57761aab75e00ef0e85082898bcdf2783
themennice <dziubiiden@gmail.com>
2020-06-05 02:55:22 -0700

    launching server

--- server.js
+++ server.js
@@ -1,15 +1,5 @@
-var http = require('http');
 const express = require('express')
 const app = express()
-
-//create a server object:
-http.createServer(function (req, res) {
-  console.log("Listening on port 8080");
-  res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
-
-
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
