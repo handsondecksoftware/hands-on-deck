1da9f2526d21a429cf0fc584ce27bf4808da4604
rstolys <44356584+rstolys@users.noreply.github.com>
2020-06-26 10:34:30 -0700

    Sending to denys

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-var http = require('http');
+//var http = require('http');
 
 //create a server object:
-http.createServer(function (req, res) {
-  console.log("Listening on port 8080");
-  res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
+//http.createServer(function (req, res) {
+ // console.log("Listening on port 8080");
+  //res.write('Hello World! - ryan '); //write a response to the client
+ // res.sendFile('index.html');
+  //res.http(index.html);
+ // res.end(); //end the response
+//}).listen(8080); //the server object listens on port 8080
 
 //This is a test comment 
+
+const express = require('express');
+const app = new express();
+
+app.get('/', function(request, response){
+    response.sendFile('index.html');
+});
