eef7b857a51e409f6609d1817ced5abac93400a1
themennice <dziubiiden@gmail.com>
2020-06-26 10:46:54 -0700

    linked express

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
-//var http = require('http');
-
-//create a server object:
-//http.createServer(function (req, res) {
- // console.log("Listening on port 8080");
-  //res.write('Hello World! - ryan '); //write a response to the client
- // res.sendFile('index.html');
-  //res.http(index.html);
- // res.end(); //end the response
-//}).listen(8080); //the server object listens on port 8080
-
 //This is a test comment 
 
-const express = require('express');
-const app = new express();
+var express = require('express')
+const app = express();
+var http = require('http');
+
+var PORT = 8080;
+
+var server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 app.get('/', function(request, response){
-    response.sendFile('index.html');
+    response.sendFile(__dirname + '/index.html');
 });
