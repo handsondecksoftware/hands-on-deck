cde04134eedb4e49fb13da8a58339d87df145f4e
rstolys <44356584+rstolys@users.noreply.github.com>
2020-06-27 11:58:27 -0700

    Created File Structure
    Generated files we are going to use and a general file and comment structure to follow

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
-//This is a test comment 
+////////////////////////////////////////////////////////////
+// server.js -- starts the server to allow for local devlopment 
+//              and editting
+//                   
+// Denys Dziubii,	26/06/20
+//    - File Created and configured
+//
+////////////////////////////////////////////////////////////
 
 var express = require('express')
 const app = express();
@@ -7,5 +14,5 @@ var http = require('http');
 var server = app.listen(process.env.PORT || 5000, () => console.log(`Listening on 5000`));
 
 app.get('/', function(request, response){
-    response.sendFile(__dirname + '/index.html');
+    response.sendFile(__dirname + '/html_files/index.html');
 });
