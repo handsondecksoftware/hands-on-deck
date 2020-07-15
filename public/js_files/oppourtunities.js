4a7c83ef838e2817989191068c83d4a31ae91f27
rstolys <44356584+rstolys@users.noreply.github.com>
2020-07-15 16:10:58 -0700

    Upadting layout for webpages
    Added popup box functionality for oppourtunties page and updating general layout for each page

---
+++ public/js_files/oppourtunities.js
@@ -0,0 +1,58 @@
+////////////////////////////////////////////////////////////////////////
+// oppourtunities.js -- frontend behaviour for oppourtunities page
+//                  
+//
+// Ryan Stolys, 15/07/20
+//    - File Created
+//    - Intial behaviour 
+//
+////////////////////////////////////////////////////////////////////////
+
+
+//Add DOM elements 
+function init()
+  {
+  //Setup open and close of popup box
+  document.getElementById('addOppourtunityButton').onclick = function(){toggleOppourtuntiyBoxVisibility()};
+  document.getElementById('cancelOppourtunityChoice').onclick = function(){toggleOppourtuntiyBoxVisibility()};
+  document.getElementById('addOppourtunityChoice').onclick = function(){addOppourtunity()};
+  
+  
+  }
+
+
+
+////////////////////////////////////////////////////////////////////////
+// 
+// Will eiter display or hide the add oppourtuntiy box depending on the current state
+//
+////////////////////////////////////////////////////////////////////////
+function toggleOppourtuntiyBoxVisibility()
+  {
+  //Open the add oppourtuntiy popup box
+  var currentState = document.getElementById('addOppourtunityPopup').style.display; 
+
+  if(currentState === "none")
+    {
+    document.getElementById('addOppourtunityPopup').style.display = "block"; 
+    }
+  else 
+    {
+    document.getElementById('addOppourtunityPopup').style.display = "none"; 
+    }
+
+  return;
+  }
+
+
+////////////////////////////////////////////////////////////////////////
+// 
+// Will check inputs and add oppourtuntiy if valid, or provide errors if not
+//
+// currently does nothing, just closes  the oppourtunity box
+//
+////////////////////////////////////////////////////////////////////////
+function addOppourtunity()
+  {
+  toggleOppourtuntiyBoxVisibility();
+  }
