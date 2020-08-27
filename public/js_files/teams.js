13631507955e8a513aa05aee0a039232d28d6c03
JaydenCole-55 <jaydenc@sfu.ca>
2020-08-27 11:10:07 -0700

    Added specific team view from all teams page

--- public/js_files/teams.js
+++ public/js_files/teams.js
@@ -16,7 +16,7 @@
 //////////////////////////////////////////////////////////////////////// 
 function init()
   {
-  //Setup open and close of popup box
+  //Setup open and close of add team popup box
   document.getElementById('addTeamButton').onclick = function(){toggleTeamBoxVisibility()};
   document.getElementById('cancelTeamChoice').onclick = function(){toggleTeamBoxVisibility()};
   document.getElementById('addTeamChoice').onclick = function(){addTeam()};
@@ -24,7 +24,8 @@ function init()
   document.getElementById('editViewVolunteer').onclick = function(){toggleViewVolunteerBoxVisibility()}; //Need to edit volunteer on this click
   document.getElementById('cancelViewVolunteer').onclick = function(){toggleViewVolunteerBoxVisibility()};
   document.getElementById('viewVolunteerInfo').onclick = function(){toggleViewVolunteerBoxVisibility()};
-  
+
+  document.getElementById('backToAllTeamsButton').onclick = function(){toggleViewTeams()};
   
   initSlider('Teams');
 
@@ -119,4 +120,53 @@ function deleteTeam(e){
         }
     }
     return false;
+}
+
+
+///////////////////////////////////////////////////////////////////////
+// 
+// View a specific team
+//
+////////////////////////////////////////////////////////////////////////
+function viewTeam(e){
+    // Switch view to the specific team page
+    document.getElementById("teamMainPage").style.display = "none";
+    document.getElementById("viewTeamPage").style.display = "block";
+
+    // Determines the team
+    e = e || event;
+    var eventEl = e.srcElement || e.target, 
+    parent = eventEl.parentNode,
+    isRow = function(el) {
+                return el.tagName.match(/tr/i);
+            };
+
+    // Move up the DOM until tr is reached
+    while (parent = parent.parentNode) {
+        if (isRow(parent)) {
+            // Get team name and gender from the clicked row
+            var teamName = parent.cells.item(0).innerHTML;
+            var sex = parent.cells.item(1).innerHTML;
+
+            document.getElementById("teamInfo-name").innerHTML = teamName;
+            document.getElementById("teamInfoSexLabel").innerHTML = sex;
+
+            //TODO: Get volunteers from the team and fill out team's table
+
+           return true;
+        }
+    }
+    return false;
+}
+
+////////////////////////////////////////////////////////////////////////
+//
+// This function will take a user from viewing a specific team to all teams
+// from the institution
+//
+////////////////////////////////////////////////////////////////////////
+function toggleViewTeams(){
+    document.getElementById("teamMainPage").style.display = "block";
+    document.getElementById("viewTeamPage").style.display = "none";
+    return;
 }
