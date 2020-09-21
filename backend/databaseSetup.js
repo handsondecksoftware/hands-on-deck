c3c36ff3afb60dda1955b52729c20fb7b55cea5e
rstolys <44356584+rstolys@users.noreply.github.com>
2020-09-20 21:12:08 -0700

    Server.js updates
    Cleaning up signin and database accesses

--- backend/databaseSetup.js
+++ backend/databaseSetup.js
@@ -6,9 +6,10 @@
 //
 ////////////////////////////////////////////////////////////////////////
 
-const { Client } = require('pg');
-
-const client = new Client(
+const { Pool, Client } = require('pg');
+const { exit } = require('process');
+const { SSL_OP_NO_SESSION_RESUMPTION_ON_RENEGOTIATION } = require('constants');
+const pool = new Pool(
   {
   connectionString: "postgres://kuskxdbbzhvwkz:68cbfc9d44fbc241c4f3e26a56327d009f5f6e4b75d04a7c0874e9b2536c1ade@ec2-3-222-30-53.compute-1.amazonaws.com:5432/d8sc0ku4m33dnj", //process.env.DATABASE_URL,  //This is undefined. We  need to insert the actual URL -- I couldnt find it
   ssl: 
@@ -17,6 +18,37 @@ const client = new Client(
     }
   });
 
-client.connect();
+//exports.db = client;
 
-exports.db = client;
+
+////////////////////////////////////////////////////////////////////////
+// 
+// This will handle all database queries indirectly                
+//
+////////////////////////////////////////////////////////////////////////
+exports.queryDB = async (queryString, callbackFunction) => {
+  
+  pool.connect().then(client => 
+    {
+    return client.query(queryString).then(res => 
+        {
+        client.release()
+        callbackFunction(false, res);
+        })
+      .catch(e => 
+        {
+        client.release()
+        console.log('An Error occured');
+        });
+    });
+
+
+  return 
+}
+
+
+
+
+
+
+
