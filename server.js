8615c6c11116f98e42a5fd984e39c3dbda6d4103
themennice <dziubiiden@gmail.com>
2021-02-17 20:17:25 -0800

    saving progress on jwt authentication

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ const LocalStrategy = require('passport-local').Strategy; // strategy for authen
 const session = require('express-session');
 const bcrypt = require('bcryptjs');
 
+const jwt = require('jsonwebtoken');
 ////////////////////////////////////////////////////////////////////////
 // GLOABL CONSTANTS AND VARIABLES
 ////////////////////////////////////////////////////////////////////////
@@ -302,7 +303,91 @@ app.get('/api/createAccount', async (request, response) =>
     response.send({success: false, errormessage: " Request Recieved. Not yet implemented."});
     });
 
+
+
 app.get('/api/signIn', function(request, response, next) {
+    const email = request.query.username;
+    const password = request.query.password;
+    console.log(request.query)
+    const user = {
+        email: email,
+        password: password
+    }
+
+    try {
+          console.log("CURRENT USERNAME IS " + email);
+          database.queryDB('SELECT volunteer_id, institution_id, email, password, volunteertype FROM volunteer WHERE email=\'' + email + '\';', function (result, err) {
+    
+              if (err) {
+                console.log('Error Occured: ');
+                console.log(err);
+                response.send(err);
+              }
+  
+              if (result.rows[0] == null) {
+                console.log("Oops. Incorrect login details.");
+                response.send(null, false, { message: 'Incorrect Email' });
+              }
+              else {
+  
+                  bcrypt.compare(password, result.rows[0].password, function (err, isMatch) {
+                    if (err) throw err;
+                    else if (isMatch) {
+                        console.log("Passwords matched!");
+                        //if user log in success, generate a JWT token for the user with a secret key
+                        jwt.sign({user}, 'privatekey', { expiresIn: '1h' },(err, token) => {
+                            if(err) { console.log(err) }    
+                            response.send(token);
+                        })
+                        //return done(null, [{ institution_id: result.rows[0].institution_id, volunteer_id: result.rows[0].volunteer_id, volunteertype: result.rows[0].volunteertype}]);
+                      }
+                      else {
+                        console.log("Oops. Incorrect login details.");
+                        response.send(null, false, { message: 'Incorrect Password' });
+                        }
+                    });
+                };
+          })
+         }
+        catch (e) { throw (e); }
+
+})
+//Check to make sure header is not undefined, if so, return Forbidden (403)
+const checkToken = (req, res, next) => {
+    const header = req.headers['authorization'];
+
+    if(typeof header !== 'undefined') {
+        const bearer = header.split(' ');
+        const token = bearer[1];
+
+        req.token = token;
+        next();
+    } else {
+        //If header is undefined return Forbidden (403)
+        res.sendStatus(403)
+    }
+}
+
+//This is a protected route 
+app.get('/user/data', checkToken, (req, res) => {
+    //verify the JWT token generated for the user
+    jwt.verify(req.token, 'privatekey', (err, authorizedData) => {
+        if(err){
+            //If error send Forbidden (403)
+            console.log('ERROR: Could not connect to the protected route');
+            res.sendStatus(403);
+        } else {
+            //If token is successfully verified, we can send the autorized data 
+            res.json({
+                message: 'Successful log in',
+                authorizedData
+            });
+            console.log('SUCCESS: Connected to protected route');
+        }
+    })
+});
+
+app.get('/api/signIndummy', function(request, response, next) {
     passport.authenticate('local', (err, user, info) => {
     if (err) { 
         return next(err); 
