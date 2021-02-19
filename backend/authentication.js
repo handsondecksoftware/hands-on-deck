a1f8171085b9ec313bc4d58719d53fc1fda135e0
rstolys <44356584+rstolys@users.noreply.github.com>
2021-02-18 20:34:31 -0800

    Revert "Denys"

--- backend/authentication.js
+++ backend/authentication.js
@@ -14,28 +14,27 @@ const error = require('./errorCodes');
 //
 // This will ensure user is authenticated before handling request.  
 //
-// Access Control will be expanded to include passport and auth check, for now, this will do.
+// Access Control will be exapanded to include passport and auth check, for now, this will do.
 // this function will be used to make sure only logged in users have access to sensitive information
 //
 ////////////////////////////////////////////////////////////
 exports.authcheck_get = (req, res, next) => 
     {
-    // const header = req.headers['tokenKey'];
-    const header = req.cookies.tokenKey
-    if(typeof header !== 'undefined') {
-        req.token = header;
-        console.log(req.token)
+    if (req.isAuthenticated()) 
+        {
         return next();   // if the user is logged in, proceed to the next function, as needed
         }
     else            // otherwise, redirect to sign in page and send the message below   
+        {
         res.render('pages/signIn', { message: 'Not authorized, please login' });
+        }
     }
 
 ////////////////////////////////////////////////////////////
 //
 // This will ensure user is authenticated before handling request.  
 //
-// Access Control will be expanded to include passport and auth check, for now, this will do.
+// Access Control will be exapanded to include passport and auth check, for now, this will do.
 // this function will be used to make sure only logged in users have access to sensitive information
 //
 ////////////////////////////////////////////////////////////
